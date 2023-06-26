2441fed0e64074442aeca657ac9afc0407ea0a9b
comorbidity <andrew.mcmurry@tch.harvard.edu>
2023-06-26 16:22:24 -0700

    defined study period.

---
+++ cumulus_library_suicide_icd10/suicide_icd10/define_study_period.sql
@@ -0,0 +1,7 @@
+create or replace view suicide_icd10__define_period as select * from (VALUES
+  ('before-covid', date('2016-06-01'), date('2020-03-01'))
+ ,('before-delta', date('2020-03-01'), date('2021-06-20'))
+ ,('delta',     date('2021-06-21'), date('2021-12-19'))
+ ,('omicron',   date('2021-12-20'), date('2022-06-01')))
+AS t (period, period_start, period_end)
+;
