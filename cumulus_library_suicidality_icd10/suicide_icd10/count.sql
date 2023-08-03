cb0fd17a943a2f2545a00c57d473bfbbc568eb4a
comorbidity <andrew.mcmurry@tch.harvard.edu>
2023-08-02 17:32:27 -0700

    suicidality prevalence now included in count.sql and manifest.toml

--- cumulus_library_suicidality_icd10/suicide_icd10/count.sql
+++ cumulus_library_suicidality_icd10/suicide_icd10/count.sql
@@ -1,56 +1,37 @@
 -- ###########################################################
-CREATE TABLE suicide_icd10__count_dx_week AS 
-    with powerset as
-    (
-        select
-        count(distinct subject_ref)   as cnt_subject
-        , count(distinct encounter_ref)   as cnt_encounter
-        , cond_week, dx_subtype, dx_display, doc_ed_note, gender, race_display, age_at_visit, enc_class_code, doc_type_display        
-        FROM suicide_icd10__dx
-        group by CUBE
-        ( cond_week, dx_subtype, dx_display, doc_ed_note, gender, race_display, age_at_visit, enc_class_code, doc_type_display )
-    )
-    select
-          cnt_encounter  as cnt 
-        , cond_week, dx_subtype, dx_display, doc_ed_note, gender, race_display, age_at_visit, enc_class_code, doc_type_display
-    from powerset 
-    WHERE cnt_subject >= 5 
-    ORDER BY cnt desc;
-
--- ###########################################################
 CREATE TABLE suicide_icd10__count_dx_month AS 
     with powerset as
     (
         select
         count(distinct subject_ref)   as cnt_subject
         , count(distinct encounter_ref)   as cnt_encounter
-        , cond_month, dx_subtype, dx_display, doc_ed_note, gender, race_display, age_at_visit, enc_class_code, doc_type_display        
+        , cond_month, dx_subtype, dx_display, gender, race_display, age_at_visit, enc_class_code, doc_ed_note, doc_type_display        
         FROM suicide_icd10__dx
         group by CUBE
-        ( cond_month, dx_subtype, dx_display, doc_ed_note, gender, race_display, age_at_visit, enc_class_code, doc_type_display )
+        ( cond_month, dx_subtype, dx_display, gender, race_display, age_at_visit, enc_class_code, doc_ed_note, doc_type_display )
     )
     select
           cnt_encounter  as cnt 
-        , cond_month, dx_subtype, dx_display, doc_ed_note, gender, race_display, age_at_visit, enc_class_code, doc_type_display
+        , cond_month, dx_subtype, dx_display, gender, race_display, age_at_visit, enc_class_code, doc_ed_note, doc_type_display
     from powerset 
     WHERE cnt_subject >= 5 
     ORDER BY cnt desc;
 
 -- ###########################################################
-CREATE TABLE suicide_icd10__count_study_period_week AS 
+CREATE TABLE suicide_icd10__count_prevalence_month AS 
     with powerset as
     (
         select
         count(distinct subject_ref)   as cnt_subject
         , count(distinct encounter_ref)   as cnt_encounter
-        , enc_start_week, period, gender, age_group, race_display, doc_ed_note, enc_class_code, doc_type_display        
-        FROM suicide_icd10__study_period
+        , enc_start_month, dx_subtype, period, gender, age_group, doc_ed_note, enc_class_code        
+        FROM suicide_icd10__prevalence
         group by CUBE
-        ( enc_start_week, period, gender, age_group, race_display, doc_ed_note, enc_class_code, doc_type_display )
+        ( enc_start_month, dx_subtype, period, gender, age_group, doc_ed_note, enc_class_code )
     )
     select
           cnt_encounter  as cnt 
-        , enc_start_week, period, gender, age_group, race_display, doc_ed_note, enc_class_code, doc_type_display
+        , enc_start_month, dx_subtype, period, gender, age_group, doc_ed_note, enc_class_code
     from powerset 
     WHERE cnt_subject >= 5 
     ORDER BY cnt desc;
