f7640b75bf21d736bfc1e904cf8341bb0ddcfaef
comorbidity <andrew.mcmurry@tch.harvard.edu>
2023-08-02 17:31:49 -0700

    initial import suicidality prevalence

---
+++ cumulus_library_suicidality_icd10/suicide_icd10/table_prevalence.sql
@@ -0,0 +1,11 @@
+drop table if exists suicide_icd10__prevalence;
+
+create TABLE suicide_icd10__prevalence AS
+    select distinct
+        coalesce(DX.dx_subtype, 'None') as dx_subtype,
+        coalesce(DX.dx_code, 'None') as dx_code,
+        coalesce(DX.dx_display, 'None') as dx_display,
+        coalesce(DX.dx_code_display, 'None') as dx_code_display,
+        S.*
+    from suicide_icd10__study_period as S
+    left join suicide_icd10__dx as DX on S.encounter_ref = DX.encounter_ref;
