cfaa78759f22e98f28208b63e51026d00505e65e
comorbidity <andrew.mcmurry@tch.harvard.edu>
2023-08-02 17:01:12 -0700

    fixed typo in define_note.sql

--- cumulus_library_suicidality_icd10/suicide_icd10/define_note.sql
+++ cumulus_library_suicidality_icd10/suicide_icd10/define_note.sql
@@ -3,7 +3,7 @@ create or replace view suicide_icd10__define_note as select * from
         ('BCH', 'NOTE:149798455', 'Emergency MD',                         'http://loinc.org','34878-9', 'Emergency medicine Note')
       , ('BCH', 'NOTE:189094716', 'Psychiatry Evaluation',                'http://loinc.org','97696-9', 'Psychiatry Evaluation note')
       , ('BCH', 'NOTE:262402257', 'Psychiatry Evaluation Consultation',   'http://loinc.org','34788-0', 'Psychiatry Consult note')
-      , ('BCH', 'NOTE:3710478',   'Discharge Summary',                   'http://loinc.org','18842-5', '18842-5 Discharge summary')
+      , ('BCH', 'NOTE:3710478',   'Discharge Summary',                   'http://loinc.org','18842-5',  'Discharge summary')
 )
 as t(from_system, from_code, from_display, system, code, display)
 ;
