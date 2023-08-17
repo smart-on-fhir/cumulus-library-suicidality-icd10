31e19bc4e9b334cc316356498cdc5333b68caf12
Matt Garber <matthew.garber@childrens.harvard.edu>
2023-08-17 11:41:06 -0400

    Update for new condition table

--- cumulus_library_suicidality_icd10/suicide_icd10/table_dx.sql
+++ cumulus_library_suicidality_icd10/suicide_icd10/table_dx.sql
@@ -1,9 +1,4 @@
 create TABLE suicide_icd10__dx as
-with flat_condition as
-(
-    select * from core__condition,
-    unnest(cond_code.coding) AS t1 (code_coding)
-)
     select distinct
         DX.subtype as dx_subtype,
         DX.system  as dx_system,
@@ -21,7 +16,7 @@ with flat_condition as
         S.enc_class_code,
         S.doc_type_code,
         S.doc_type_display,
-        C.category as cond_category,
+        C.code as cond_category,
         C.recorded_week as cond_week, -- FHIR Condition
         C.recorded_month as cond_month,
         C.recorded_year as cond_year,
@@ -36,10 +31,10 @@ with flat_condition as
         S.subject_ref,
         S.encounter_ref,
         S.doc_ref
-    from  flat_condition C
+    from  core__condition C
         ,   suicide_icd10__study_period S
         ,   suicide_icd10__define_dx DX
-    where C.code_coding.code = DX.code
+    where C.code = DX.code
     and   C.encounter_ref = S.encounter_ref
     and   C.subject_ref = S.subject_ref
     order by recorded_month desc, subtype, dx_code
