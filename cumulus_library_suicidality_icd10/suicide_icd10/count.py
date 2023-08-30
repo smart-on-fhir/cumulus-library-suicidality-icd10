aaf8db7c1007aa5204e024087af6844af859694c
Matt Garber <matthew.garber@childrens.harvard.edu>
2023-08-30 15:35:54 -0400

    Counts gen cutover

--- cumulus_library_suicidality_icd10/suicide_icd10/count.py
+++ cumulus_library_suicidality_icd10/suicide_icd10/count.py
@@ -1,104 +1,77 @@
-from typing import List
-from cumulus_library.schema import counts
+from pathlib import Path
+from cumulus_library.schema.counts import CountsBuilder
 
-# https://github.com/smart-on-fhir/cumulus-library-template/issues/2
-STUDY_PREFIX = 'suicide_icd10'
+class SuicideICD10CountsBuilder(CountsBuilder):
+    display_text = "Creating Suicide ICD10 counts..."
 
-def table(tablename: str, duration=None) -> str:
-    if duration:
-        return f'{STUDY_PREFIX}__{tablename}_{duration}'
-    else: 
-        return f'{STUDY_PREFIX}__{tablename}'
+    def count_dx(self, duration='month'):
+        """
+        Encounter Diagnosis, not exactly 'incidence' because DX may not be "new".
 
-def count_dx(duration='month'):
-    """
-    Encounter Diagnosis, not exactly 'incidence' because DX may not be "new".
+        Stratify variables for "documented encounter"
 
-    Stratify variables for "documented encounter"
+        :return: str suicide_icd10__count_dx_month
+        """
+        view_name = self.get_table_name('count_dx', duration)
+        from_table = self.get_table_name('dx')
+        cols = [f'cond_{duration}',
+                'dx_subtype',
+                'dx_display',
+                'gender',
+                'race_display',
+                'age_at_visit',
+                'enc_class_code',
+                'doc_ed_note',
+                'doc_type_display']
+        return self.count_encounter(view_name, from_table, cols)
 
-    :return: str suicide_icd10__count_dx_month
-    """
-    view_name = table('count_dx', duration)
-    from_table = table('dx')
-    cols = [f'cond_{duration}',
-            'dx_subtype',
-            'dx_display',
-            'gender',
-            'race_display',
-            'age_at_visit',
-            'enc_class_code',
-            'doc_ed_note',
-            'doc_type_display']
-    return counts.count_encounter(view_name, from_table, cols)
+    def count_prevalence(self, duration='month'):
+        """
+        Percentage of patients with a suicidality diagnosis
 
-def count_prevalence(duration='month'):
-    """
-    Percentage of patients with a suicidality diagnosis
+        :return: str suicide_icd10__count_prevalence_month
+        """
+        view_name = self.get_table_name('count_prevalence', duration)
+        from_table = self.get_table_name('prevalence')
+        cols = [f'enc_start_{duration}',
+                'dx_suicidality',
+                'dx_subtype',
+                'period',
+                'gender',
+                'age_group',
+                'doc_ed_note',
+                'enc_class_code']
 
-    :return: str suicide_icd10__count_prevalence_month
-    """
-    view_name = table('count_prevalence', duration)
-    from_table = table('prevalence')
-    cols = [f'enc_start_{duration}',
-            'dx_suicidality',
-            'dx_subtype',
-            'period',
-            'gender',
-            'age_group',
-            'doc_ed_note',
-            'enc_class_code']
+        return self.count_encounter(view_name, from_table, cols)
 
-    return counts.count_encounter(view_name, from_table, cols)
+    def count_study_period(self, duration='month'):
+        """
+        Frequency of documented encounters.
+        This is just the *study period* BEFORE selecting for suicidality DX
 
-def count_study_period(duration='month'):
-    """
-    Frequency of documented encounters.
-    This is just the *study period* BEFORE selecting for suicidality DX
+        :return: str suicide_icd10__count_study_period_month
+        """
+        view_name = self.get_table_name('count_study_period', duration)
+        from_table = self.get_table_name('study_period')
+        cols = [f'enc_start_{duration}',
+                'period',
+                'gender',
+                'age_group',
+                'race_display',
+                'doc_ed_note',
+                'enc_class_code',
+                'doc_type_display']
 
-    :return: str suicide_icd10__count_study_period_month
-    """
-    view_name = table('count_study_period', duration)
-    from_table = table('study_period')
-    cols = [f'enc_start_{duration}',
-            'period',
-            'gender',
-            'age_group',
-            'race_display',
-            'doc_ed_note',
-            'enc_class_code',
-            'doc_type_display']
-
-    return counts.count_encounter(view_name, from_table, cols)
-
-def concat_view_sql(create_view_list: List[str]) -> str:
-    """
-    :param create_view_list: SQL prepared statements
-    :param filename: path to output file, default 'count.sql' in PWD
-    """
-    seperator = '-- ###########################################################'
-    concat = list()
-
-    for create_view in create_view_list:
-        concat.append(seperator + '\n'+create_view + '\n')
-
-    return '\n'.join(concat)
-
-def write_view_sql(view_list_sql: List[str], filename='count.sql') -> None:
-    """
-    :param view_list_sql: SQL prepared statements
-    :param filename: path to output file, default 'count.sql' in PWD
-    """
-    sql_optimizer = concat_view_sql(view_list_sql)
-    sql_optimizer = sql_optimizer.replace("CREATE or replace VIEW", 'CREATE TABLE')
-    sql_optimizer = sql_optimizer.replace('WHERE cnt_subject >= 10', 'WHERE cnt_subject >= 5')
-
-    with open(filename, 'w') as fout:
-        fout.write(sql_optimizer)
+        return self.count_encounter(view_name, from_table, cols)
 
 
-if __name__ == '__main__':
-    write_view_sql([
-        count_dx(),
-        count_prevalence(),
-        count_study_period()
-    ])
+    def prepare_queries(self, cursor=None, schema=None):
+        self.queries=[
+            self.count_dx(),
+            self.count_prevalence(),
+            self.count_study_period()
+        ]
+
+if __name__ == "__main__":
+    builder = SuicideICD10CountsBuilder()
+    builder.write_counts(f"{Path(__file__).resolve().parent}/count.sql")
--- cumulus_library_suicidality_icd10/suicide_icd10/count.sql
+++ cumulus_library_suicidality_icd10/suicide_icd10/count.sql
@@ -1,56 +1,134 @@
--- ###########################################################
-CREATE TABLE suicide_icd10__count_dx_month AS 
-    with powerset as
-    (
-        select
-        count(distinct subject_ref)   as cnt_subject
-        , count(distinct encounter_ref)   as cnt_encounter
-        , cond_month, dx_subtype, dx_display, gender, race_display, age_at_visit, enc_class_code, doc_ed_note, doc_type_display        
+-- noqa: disable=all
+
+CREATE TABLE suicide_icd10__count_dx_month AS (
+    WITH powerset AS (
+        SELECT
+            count(DISTINCT subject_ref) AS cnt_subject,
+            count(DISTINCT encounter_ref) AS cnt_encounter,
+            "cond_month",
+            "dx_subtype",
+            "dx_display",
+            "gender",
+            "race_display",
+            "age_at_visit",
+            "enc_class_code",
+            "doc_ed_note",
+            "doc_type_display"
         FROM suicide_icd10__dx
-        group by CUBE
-        ( cond_month, dx_subtype, dx_display, gender, race_display, age_at_visit, enc_class_code, doc_ed_note, doc_type_display )
+        GROUP BY
+            cube(
+                "cond_month",
+                "dx_subtype",
+                "dx_display",
+                "gender",
+                "race_display",
+                "age_at_visit",
+                "enc_class_code",
+                "doc_ed_note",
+                "doc_type_display"
+            )
     )
-    select
-          cnt_encounter  as cnt 
-        , cond_month, dx_subtype, dx_display, gender, race_display, age_at_visit, enc_class_code, doc_ed_note, doc_type_display
-    from powerset 
-    WHERE cnt_subject >= 5 
-    ORDER BY cnt desc;
+
+    SELECT
+        cnt_encounter AS cnt,
+        "cond_month",
+        "dx_subtype",
+        "dx_display",
+        "gender",
+        "race_display",
+        "age_at_visit",
+        "enc_class_code",
+        "doc_ed_note",
+        "doc_type_display"
+    FROM powerset
+    WHERE 
+        cnt_subject >= 10
+);
 
 -- ###########################################################
-CREATE TABLE suicide_icd10__count_prevalence_month AS 
-    with powerset as
-    (
-        select
-        count(distinct subject_ref)   as cnt_subject
-        , count(distinct encounter_ref)   as cnt_encounter
-        , enc_start_month, dx_suicidality, dx_subtype, period, gender, age_group, doc_ed_note, enc_class_code        
+
+CREATE TABLE suicide_icd10__count_prevalence_month AS (
+    WITH powerset AS (
+        SELECT
+            count(DISTINCT subject_ref) AS cnt_subject,
+            count(DISTINCT encounter_ref) AS cnt_encounter,
+            "enc_start_month",
+            "dx_suicidality",
+            "dx_subtype",
+            "period",
+            "gender",
+            "age_group",
+            "doc_ed_note",
+            "enc_class_code"
         FROM suicide_icd10__prevalence
-        group by CUBE
-        ( enc_start_month, dx_suicidality, dx_subtype, period, gender, age_group, doc_ed_note, enc_class_code )
+        GROUP BY
+            cube(
+                "enc_start_month",
+                "dx_suicidality",
+                "dx_subtype",
+                "period",
+                "gender",
+                "age_group",
+                "doc_ed_note",
+                "enc_class_code"
+            )
     )
-    select
-          cnt_encounter  as cnt 
-        , enc_start_month, dx_suicidality, dx_subtype, period, gender, age_group, doc_ed_note, enc_class_code
-    from powerset 
-    WHERE cnt_subject >= 5 
-    ORDER BY cnt desc;
+
+    SELECT
+        cnt_encounter AS cnt,
+        "enc_start_month",
+        "dx_suicidality",
+        "dx_subtype",
+        "period",
+        "gender",
+        "age_group",
+        "doc_ed_note",
+        "enc_class_code"
+    FROM powerset
+    WHERE 
+        cnt_subject >= 10
+);
 
 -- ###########################################################
-CREATE TABLE suicide_icd10__count_study_period_month AS 
-    with powerset as
-    (
-        select
-        count(distinct subject_ref)   as cnt_subject
-        , count(distinct encounter_ref)   as cnt_encounter
-        , enc_start_month, period, gender, age_group, race_display, doc_ed_note, enc_class_code, doc_type_display        
+
+CREATE TABLE suicide_icd10__count_study_period_month AS (
+    WITH powerset AS (
+        SELECT
+            count(DISTINCT subject_ref) AS cnt_subject,
+            count(DISTINCT encounter_ref) AS cnt_encounter,
+            "enc_start_month",
+            "period",
+            "gender",
+            "age_group",
+            "race_display",
+            "doc_ed_note",
+            "enc_class_code",
+            "doc_type_display"
         FROM suicide_icd10__study_period
-        group by CUBE
-        ( enc_start_month, period, gender, age_group, race_display, doc_ed_note, enc_class_code, doc_type_display )
+        GROUP BY
+            cube(
+                "enc_start_month",
+                "period",
+                "gender",
+                "age_group",
+                "race_display",
+                "doc_ed_note",
+                "enc_class_code",
+                "doc_type_display"
+            )
     )
-    select
-          cnt_encounter  as cnt 
-        , enc_start_month, period, gender, age_group, race_display, doc_ed_note, enc_class_code, doc_type_display
-    from powerset 
-    WHERE cnt_subject >= 5 
-    ORDER BY cnt desc;
+
+    SELECT
+        cnt_encounter AS cnt,
+        "enc_start_month",
+        "period",
+        "gender",
+        "age_group",
+        "race_display",
+        "doc_ed_note",
+        "enc_class_code",
+        "doc_type_display"
+    FROM powerset
+    WHERE 
+        cnt_subject >= 10
+);
